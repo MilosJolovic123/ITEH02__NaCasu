b5457ff04a856069eeb6d9e046f4b296e23f9023
Milos <jolovicmilos8@gmail.com>
2025-11-14 14:50:47 +0100

    prvi commit sa casa 2

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const x = 5;
+
+console.log(x)
+
+
+// var i let
+
+let y = 10;
+y = "Dobar dan";
+console.log(y);
+
+console.log(z);
+
+var z = 20;
+
+/*
+var z;
+console.log(z)
+z = 20;
+*/
+
+const form = document.getElementById("form-id");
+
+console.log(form);
+//kada uzimamo preko klase vraca se HTMLCollection
+const table_body = document.getElementsByClassName("table-body")[0];
